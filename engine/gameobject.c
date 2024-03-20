e05091195790ab2e595e87d30a150da0c5d9f8d7
Humza Qureshi <humzaqureshis@gmail.com>
2024-03-20 11:15:04 -0500

    gameobject and scripts

--- engine/gameobject.c
+++ engine/gameobject.c
@@ -4,7 +4,32 @@
 #include "transform.h"
 #include "gameobject.h"
 
-enum fluxScriptID{
+struct fluxScriptStruct;
+typedef struct fluxScriptStruct* fluxScript;
+
+// game object struct
+struct fluxGameObjectStruct{
+    // unique ID of gameobject
+    int id;
+    // the tag of the gameobject
+    const char* tag;
+    // the transform of the object
+    struct fluxTransform transform;
+    // the model of the object, if `has_model`
+    bool has_model;
+    Model model;
+    // the camera of the object, if `!has_model` and `is_camera`
+    bool is_camera;
+    Camera3D cam;
+    // scripts associated with this gameobject
+    int n_scripts;
+    fluxScript scripts[FLUX_MAX_SCRIPTS];
+    // gameobjects children
+    int n_children;
+    struct fluxGameObjectStruct* children[FLUX_MAX_CHILDREN];
+};
+
+/*enum fluxScriptID{
     RAND,WHO,CARES
 };
 
@@ -30,4 +55,4 @@ struct fluxGameObjectPrefabStruct{
     // transform to instantiate
     struct fluxTransform transform;
 
-};
+};*/
--- engine/gameobject.h
+++ engine/gameobject.h
@@ -1,11 +1,15 @@
 #ifndef _FLUX_GAMEOBJECT_H_
 #define _FLUX_GAMEOBJECT_H_
 
-// prefab of a gameobject that we can instantiate
+// gameobject struct
+struct fluxGameObjectStruct;
+typedef struct fluxGameObjectStruct* fluxGameObject;
+
+/*// prefab of a gameobject that we can instantiate
 struct fluxGameObjectPrefabStruct;
 typedef struct fluxGameObjectPrefabStruct* fluxGameObjectPrefab;
 
 struct fluxGameObjectStruct;
-typedef struct fluxGameObjectStruct* fluxGameObject;
+typedef struct fluxGameObjectStruct* fluxGameObject;*/
 
 #endif
